952e7c2ca9da1f6a2be58cc82a7332d9c8225e3a
singhv6899 <123750028+singhv6899@users.noreply.github.com>
2024-07-29 11:26:44 -0400

    first commit

---
+++ taskmanager-django/taskmanager/tasks/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    @action(detail=True, methods=['post'])
+    def mark_complete(self, request, pk=None):
+        task = self.get_object()
+        task.completed = True
+        task.save()
+        return Response({'status': 'task marked as complete'})
